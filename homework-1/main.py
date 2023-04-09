747fb6d1cf0c05f165a6ef31baffead8dda6563b
Александр Хасматулин <alexandr.khasmatulin@yandex.ru>
2023-04-09 12:21:08 +0300

    final

--- homework-1/main.py
+++ homework-1/main.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
     print(item2.calculate_total_price())  # 100000
 
     # устанавливаем новый уровень цен
-    Item.pay_rate = 0.8
     # применяем скидку
-    item1.apply_discount()
+    Item.pay_rate = 0.8
+    item1.price = item1.apply_discount()
 
     print(item1.price)  # 8000.0
     print(item2.price)  # 20000
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import unittest
+from src.item import Item
+
+class testItem(unittest.TestCase):
+    def test_calculate_total_price(self):
+        item1 = Item('Смартфон', 10000, 20)
+        self.assertEqual(item1.calculate_total_price(), 200000)
+
+    def test_apply_discount(self):
+        item1 = Item('Смартфон', 10000, 20)
+        Item.pay_rate = 0.8
+        self.assertEqual(item1.apply_discount(), 8000)
+if __name__ == '__main__':
+    unittest.main()
